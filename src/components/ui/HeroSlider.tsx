84613e83e69a55497059d23640664d8902bed9cf
slsuyel <slsuyel@gmail.com>
2024-12-22 20:07:40 +0600

    feat: update the hero image

--- src/components/ui/HeroSlider.tsx
+++ src/components/ui/HeroSlider.tsx
@@ -2,10 +2,8 @@ import { Carousel } from "antd";
 
 const images = [
   "https://tds-images.thedailystar.net/sites/default/files/images/2022/12/12/digital_bangladesh.jpg",
-  "https://tds-images.thedailystar.net/sites/default/files/styles/very_big_201/public/images/2023/01/16/digital_bangladesh.png",
-  "https://tds-images.thedailystar.net/sites/default/files/feature/images/smart-city_myth.jpg",
-  "https://untoldmag.org/wp-content/uploads/2024/08/Palestine-Flag-held-high-in-July-Movement.jpeg",
-  "https://www.tbsnews.net/sites/default/files/styles/infograph/public/images/2022/06/28/impact-of-the-budget-proposals-info.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Government_of_Bangladesh_Logo_%28unofficial_and_fictional_logo%29.png/1029px-Government_of_Bangladesh_Logo_%28unofficial_and_fictional_logo%29.png",
+  "https://tds-images.thedailystar.net/sites/default/files/images/2022/12/12/digital_bangladesh.jpg",
 ];
 
 const HeroSlider = () => {
@@ -13,7 +11,11 @@ const HeroSlider = () => {
     <Carousel autoplay effect="fade" dots={false} className="services p-0">
       {images.map((imageUrl, index) => (
         <div key={index}>
-          <img src={imageUrl} alt="" className="w-100 hero_slider_img" />
+          <img
+            src={imageUrl}
+            alt=""
+            className="w-100 hero_slider_img object-fit-cover"
+          />
         </div>
       ))}
     </Carousel>
