5a8e6db2c3fc967e7f37ac7678b013e267355fb1
slsuyel <slsuyel@gmail.com>
2025-01-01 21:53:50 +0600

    feat: change color

--- src/pages/dashboard/Admin/Summary.tsx
+++ src/pages/dashboard/Admin/Summary.tsx
@@ -8,13 +8,15 @@ const SummaryItem = ({
   value: number;
 }) => {
   return (
-    <div className="col-md-3 my-1 font_amazon">
-      <div className="border-0 card hov-card mb-2 py-3 shadow-sm">
-        <div className="card-body d-flex align-item-center justify-content-around">
-          <i className={`p-3 rounded-circle my-auto ${icon} fs-1`}></i>
+    <div className="col-md-3 my-2">
+      <div className="border-0 card h-100 mb-2 py-3 shadow-sm hover-effect">
+        <div className="card-body d-flex align-items-center justify-content-around">
+          <i
+            className={`p-3 rounded-circle my-auto ${icon} fs-1 bg-light text-primary`}
+          ></i>
           <div className="text-end">
-            <h5 className="card-title fw-bold ">{title}</h5>
-            <p className="card-text fs-4 mb-0">{value}</p>
+            <h5 className="card-title fw-bold  mb-2">{title}</h5>
+            <p className="card-text fs-4 mb-0 text-primary fw-bold">{value}</p>
           </div>
         </div>
       </div>
@@ -24,19 +26,19 @@ const SummaryItem = ({
 
 const Summary = () => {
   const summaryItems = [
-    { icon: 'fa-solid fa-file-contract', title: 'মোট আবেদন', value: 15299 },
-    { icon: 'fa-solid fa-file-pen', title: 'নতুন আবেদন', value: 1126 },
-    { icon: 'fa-solid fa-file-circle-check', title: 'ইস্যুকৃত সনদ', value: 26 },
-    { icon: 'fa-solid fa-file-excel', title: 'বাতিলকৃত আবেদন', value: 500 },
+    { icon: "fa-solid fa-file-contract", title: "মোট আবেদন", value: 15299 },
+    { icon: "fa-solid fa-file-pen", title: "নতুন আবেদন", value: 1126 },
+    { icon: "fa-solid fa-file-circle-check", title: "ইস্যুকৃত সনদ", value: 26 },
+    { icon: "fa-solid fa-file-excel", title: "বাতিলকৃত আবেদন", value: 500 },
     {
-      icon: 'fa-solid fa-bangladeshi-taka-sign',
-      title: 'মোট আদায়কৃত ফি পরিমাণ',
+      icon: "fa-solid fa-bangladeshi-taka-sign",
+      title: "মোট আদায়কৃত ফি পরিমাণ",
       value: 396,
     },
   ];
 
   return (
-    <div className="row mx-auto ">
+    <div className="row mx-auto p-3">
       {summaryItems.map((item, index) => (
         <SummaryItem
           key={index}
