fd05d995289b80733eaa82f41ad188bbc3e733b0
Suyel Haque <slsuyel@gmail.com>
2025-03-15 17:10:02 +0600

    fix; endpoint change

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.unionservices.gov.bd/api',
+    baseUrl: 'https://api.uniontax.gov.bd/api',
     credentials: 'include',
   }),
   // endpoints: (builder: any) => ({}),
