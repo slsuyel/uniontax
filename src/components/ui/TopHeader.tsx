964ac3bd44854690805bf48500cf190107aefafd
Nishad Hossain <freelancernishad123@gmail.com>
2025-04-08 02:21:09 +0600

    update

--- src/components/ui/TopHeader.tsx
+++ src/components/ui/TopHeader.tsx
@@ -21,7 +21,7 @@ const TopHeader = () => {
       <div className=" container mx-auto">
         <div className="row mx-auto topHeader">
           <div className="topheaderItem col-md-6 col-6">
-            <span> ইউনিয়ন পরিষদ ক্যাশলেস সেবা সিস্টেমে স্বাগত</span>
+            <span> {site_settings?.header_title || ""}</span>
           </div>{" "}
           <div className="topheaderItem col-md-6 col-6 text-end">
             <span
